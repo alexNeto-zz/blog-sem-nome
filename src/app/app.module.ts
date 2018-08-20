8da3a4dd8aa4170ec951db5ca064a72cd2f79326
alexNeto <alex.neto3@hotmail.com>
2018-08-20 02:50:02 -0300

    feito mais layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavegacaoComponent } from './header/navegacao/navegacao.component';
+import { AutenticacaoComponent } from './header/autenticacao/autenticacao.component';
+import { FiltroComponent } from './filtro/filtro.component';
+import { FiltroPesquisaComponent } from './filtro/filtro-pesquisa/filtro-pesquisa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavegacaoComponent,
+    AutenticacaoComponent,
+    FiltroComponent,
+    FiltroPesquisaComponent
   ],
   imports: [
     BrowserModule,
