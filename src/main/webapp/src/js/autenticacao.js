27ec80924a7ce49f1d62160fdeda070a894e78f1
alexNeto <alex.neto3@hotmail.com>
2018-11-20 11:11:53 -0200

    feito post separado

--- src/main/webapp/src/js/autenticacao.js
+++ src/main/webapp/src/js/autenticacao.js
@@ -122,20 +122,24 @@ function sair() {
 /*************** EVENTOS ***********************/
 /***********************************************/
 
-var modal = document.getElementById('myModal');
-var btn = document.getElementById("autenticacao");
-var span = document.getElementsByClassName("fecha")[0];
+(() => controlaModal())();
 
-btn.onclick = function () {
-    modal.style.display = "block";
-}
+function controlaModal() {
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("autenticacao");
+    var span = document.getElementsByClassName("fecha")[0];
 
-span.onclick = function () {
-    modal.style.display = "none";
-}
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
 
-window.onclick = function (event) {
-    if (event.target == modal) {
+    span.onclick = function () {
         modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
 }
--- src/main/webapp/src/js/httpServico.js
+++ src/main/webapp/src/js/httpServico.js
@@ -1,6 +1,7 @@
 "use strict";
 
 function get(url, dado, sucesso, falha) {
+    consola(url);
     $.ajax({
         url: pegaUrl() + url,
         data: dado,
@@ -11,6 +12,7 @@ function get(url, dado, sucesso, falha) {
 }
 
 function post(url, dado, sucesso, falha) {
+    consola(url);
     $.ajax({
         url: pegaUrl() + url,
         data: dado,
@@ -20,14 +22,15 @@ function post(url, dado, sucesso, falha) {
     });
 }
 
+function consola(url) {
+    console.log(pegaUrl() + url)
+}
+
 function erro(xhr, status, error) {
     console.log("TODO - implements");
     console.log(xhr, status, error);
 }
 
 function pegaUrl() {
-    return window.location.protocol +
-        "//" +
-        window.location.host +
-        window.location.pathname;
+    return window.location.protocol + "//" + window.location.host + "/blog/";
 }
